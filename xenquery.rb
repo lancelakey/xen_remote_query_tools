1fb91e9e8d1222befdce1dc3a2e39a9a92086802
Lance Lakey <lancelakey@gmail.com>
2011-11-30 18:17:30 -0800

    use gem highline for menu

--- xenquery.rb
+++ xenquery.rb
@@ -1,7 +1,12 @@
 #!/usr/bin/env ruby
 
+# Author: Lance Lakey
+# Purpose: Present the user with a menu of Xen related commands to run against a group of Xen Dom0's
+# The ruby gem highline does the work of creating the menu
+
 require 'rubygems'
 require 'net/ssh'
+require 'highline/import'
 
 filename = 'hosts.txt'
 hosts_file_opened = File.open(filename)
@@ -9,15 +14,29 @@ hosts = hosts_file_opened.read()
 
 username = 'root'
 
-puts "Command examples: xm list, xen-list-images, etc."
-print "Command: "
-cmd = gets.chomp()
-puts
+# command is a local variable
+# @command is an instance variable
 
+choose do |menu|
+  #menu.shell  = true
+  #menu.choice(:xmlist, "xm list") do say("Good!") end
+  menu.choice(:'xm list') do |command|
+    @command = command
+  end 
+  menu.choice(:'xm info') do |command|
+    @command = command
+  end 
+  menu.choice(:'xen-list-images') do |command|
+    @command = command
+  end 
+end
+
+
+puts
 hosts.each do |host|
   begin
     Net::SSH.start( host.chomp , username, :timeout => 10) do |ssh|
-      results = ssh.exec! cmd
+      results = ssh.exec! @command
       puts "#" * 10
       puts "# Results for #{host}"
       puts "Exit Status: #{results}"
