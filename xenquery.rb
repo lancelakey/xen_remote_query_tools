661a6b0b16ee7e36eb6d6444a07519cfca4e7d6f
Lance Lakey <lancelakey@gmail.com>
2011-12-07 16:54:11 -0800

    added comments about hosts.txt and authentication

--- xenquery.rb
+++ xenquery.rb
@@ -3,6 +3,13 @@
 # Author: Lance Lakey
 # Purpose: Present the user with a menu of Xen related commands to run against a group of Xen Dom0's
 # The ruby gem highline does the work of creating the menu
+#
+# Requires a file in the same directory named hosts.txt
+# hosts.txt should contain one ip address or one hostname per line
+#
+# Authentication:
+# This script can be easily modified to use a password
+# This script currently assumes authentication with SSH keys
 
 require 'rubygems'
 require 'net/ssh'
