1a3e85905d54cbe42a584fa8b62e2fe62be3f864
Lance Lakey <lancelakey@gmail.com>
2011-12-07 18:01:28 -0800

    removed old commented highline code

--- xenquery.rb
+++ xenquery.rb
@@ -4,12 +4,12 @@
 # Purpose: Present the user with a menu of Xen related commands to run against a group of Xen Dom0's
 # The ruby gem highline does the work of creating the menu
 #
-# Requires a file in the same directory named hosts.txt
-# hosts.txt should contain one ip address or one hostname per line
+# Hosts:
+# This script reads the ip addresses or hostnames from a file in same directory named hosts.txt
+# One ip address or hostname per line
 #
 # Authentication:
-# This script can be easily modified to use a password
-# This script currently assumes authentication with SSH keys
+# This script assumes authentication with SSH keys
 
 require 'rubygems'
 require 'net/ssh'
@@ -25,8 +25,6 @@ username = 'root'
 # @command is an instance variable
 
 choose do |menu|
-  #menu.shell  = true
-  #menu.choice(:xmlist, "xm list") do say("Good!") end
   menu.choice(:'xm list') do |command|
     @command = command
   end 
